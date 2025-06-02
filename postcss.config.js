74ee8aa3629cd8f844f986aeb884e448f7e5104e
Dmytro Papiiants <papiyants@gmail.com>
2025-06-02 15:19:43 +0200

    Add scss variables and fluid function

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 import postcssPxToRem from 'postcss-pxtorem'
+
 export default ({env}) => {
-    const isProduction = env === 'production'
-    const plugins = []
+  const isProduction = env === 'production'
+  const plugins = []
 
-    if (isProduction) {
-        plugins.push(postcssPxToRem({
-            propList: ['*'],
-            mediaQuery: true,
-        }))
-    }
+  if (isProduction) {
+    plugins.push(postcssPxToRem({
+      propList: ['*'],
+      mediaQuery: true,
+    }))
+  }
 
-    return {
-        plugins,
-    }
+  return {
+    plugins,
+  }
 }

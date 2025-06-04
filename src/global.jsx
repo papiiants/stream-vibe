69103c53159c8510fdff5d087d1636f08b9340c5
Dmytro Papiiants <papiyants@gmail.com>
2025-06-04 13:17:06 +0200

    Add favicon

--- src/global.jsx
+++ src/global.jsx
@@ -14,6 +14,31 @@ export default function (props) {
     <>
       <Head htmlAttributes={{lang: 'en'}}>
         <title>Stream Vibe | {title}</title>
+        <script
+          src="/src/main"
+          type="module"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="/site.webmanifest"
+        />
       </Head>
       <Header />
       <Content>{children}</Content>

94de7ea14b07c3c79cf02d449aa55ec92c306446
Dmytro Papiiants <papiyants@gmail.com>
2025-06-05 11:19:29 +0200

    Add Logo component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return(
     (
       <header className="header">
-        <div className="header__inner container"></div>
+        <div className="header__inner container">
+          <Logo className="header__logo" loading="eager" />
+        </div>
        </header>
     )
   )

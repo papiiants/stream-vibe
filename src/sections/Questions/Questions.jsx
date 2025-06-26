8227fc4e2228cfa36b3ca92e48bd70a3a91072c0
Dmytro Papiiants <papiyants@gmail.com>
2025-06-26 17:15:34 +0200

    Add Accordion to AccordionGroup component

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import Section from "@/layouts/Section";
 import Button from "@/components/Button";
 import AccordionGroup from "@/components/AccordionGroup";
+import Accordion from "@/components/Accordion";
 
 const Questions = (props) => {
   const {
@@ -31,7 +32,17 @@ const Questions = (props) => {
         actions={<Button label="Ask a Question" href="/support" />}
       >
         <AccordionGroup columns={2}>
-          {questionItems}
+          {questionItems.map((question, index) => (
+            <Accordion
+              title={question}
+              id={`question-${index}`}
+              name="questions"
+              isOpen={index === 0}
+              key={index}
+            >
+              <p>StreamVibe is a streaming service that allows you to watch movies and shows on demand.</p>
+            </Accordion>
+          ))}
         </AccordionGroup>
       </Section>
     </div>

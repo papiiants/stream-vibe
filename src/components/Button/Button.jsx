ebe6d35ab726388d3e1b1e45af3097a9037e951f
Dmytro Papiiants <papiyants@gmail.com>
2025-07-02 10:29:50 +0200

    Fix Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -8,7 +8,7 @@ const Button = (props) => {
     href,
     target,
     /*
-    * '' (default) | 'transparent' | 'black-10'
+    * '' (default) | 'transparent' | 'black-10' | 'black-08'
     * */
     mode= '',
     type = 'button',

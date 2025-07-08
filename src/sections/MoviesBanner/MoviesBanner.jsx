d931207ecd020811d8510deef659347aed0e0c1e
Dmytro Papiiants <papiyants@gmail.com>
2025-07-08 14:17:41 +0200

    Fix MovieBanner component

--- src/sections/MoviesBanner/MoviesBanner.jsx
+++ src/sections/MoviesBanner/MoviesBanner.jsx
@@ -1,7 +1,27 @@
 import './MoviesBanner.scss'
+import MovieBannerCard from "@/components/MovieBannerCard";
+import Slider from "@/components/Slider";
 
 const MoviesBanner = () => {
   const titleId = 'movies-banner-title'
+
+  const movieCards = [
+    {
+      title: 'Avengers : Endgame',
+      description: 'With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\'s actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.',
+      imgSrc: '/src/assets/images/movie-banner/1.jpg',
+    },
+    {
+      title: 'Avengers : Endgame 1',
+      description: 'With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\'s actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.',
+      imgSrc: '/src/assets/images/movie-banner/1.jpg',
+    },
+    {
+      title: 'Avengers : Endgame 2',
+      description: 'With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\'s actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face... Avenge the fallen.',
+      imgSrc: '/src/assets/images/movie-banner/1.jpg',
+    },
+  ]
 
   return (
     <section
@@ -11,6 +31,21 @@ const MoviesBanner = () => {
       <h1 className="visually-hidden" id={titleId}>
         Movies & Shows
       </h1>
+
+      <Slider
+        sliderParams={{
+          slidesPerView: 1,
+        }}
+        navigationPosition="abs-bottom"
+      >
+        {movieCards.map((movieCard, index) => (
+          <MovieBannerCard
+            {...movieCard}
+            key={index}
+          />
+        ))}
+      </Slider>
+
     </section>
   )
 }

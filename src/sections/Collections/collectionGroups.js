a6e69e6a92473024a673e77b92404b46cd230199
Dmytro Papiiants <papiyants@gmail.com>
2025-07-12 16:13:59 +0200

    Fix add slider confit to shows items in collectionGroups

--- src/sections/Collections/collectionGroups.js
+++ src/sections/Collections/collectionGroups.js
@@ -1012,6 +1012,36 @@ const collectionGroups = [
             href: '/show'
           },
         ],
+        sliderParams: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 30,
+          breakpoints: {
+            0: {
+              slidesPerView: 1.6,
+              slidesPerGroup: 1,
+              spaceBetween: 20,
+            },
+            481: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+              spaceBetween: 20,
+            },
+            1024: {
+              spaceBetween: 20,
+              allowTouchMove: false,
+            },
+            1441: {
+              spaceBetween: 30,
+              allowTouchMove: false,
+            },
+          }
+        },
       },
       {
         title: 'New Released Shows',
@@ -1132,6 +1162,36 @@ const collectionGroups = [
             href: '/show'
           },
         ],
+        sliderParams: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 30,
+          breakpoints: {
+            0: {
+              slidesPerView: 1.6,
+              slidesPerGroup: 1,
+              spaceBetween: 20,
+            },
+            481: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+              spaceBetween: 20,
+            },
+            1024: {
+              spaceBetween: 20,
+              allowTouchMove: false,
+            },
+            1441: {
+              spaceBetween: 30,
+              allowTouchMove: false,
+            },
+          }
+        },
       },
       {
         title: 'Must - Watch Shows',

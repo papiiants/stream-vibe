5a47e77ed622c51b076bfd64b60df09ecc58c7a6
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 13:45:50 +0200

    Add small padding prop to MovieBannerCard component

--- src/components/MovieBannerCard/MovieBannerCard.jsx
+++ src/components/MovieBannerCard/MovieBannerCard.jsx
@@ -1,6 +1,7 @@
 import './MovieBannerCard.scss'
 import {Image} from "minista";
 import Button from "@/components/Button";
+import classNames from "classnames";
 
 const MovieBannerCard = (props) => {
   const {
@@ -9,7 +10,7 @@ const MovieBannerCard = (props) => {
     TitleTag = 'h2',
     description,
     imgSrc,
-
+    isSmallPaddingY = false,
   } = props
 
   return (
@@ -18,7 +19,9 @@ const MovieBannerCard = (props) => {
         className="movie-banner-card__image"
         src={imgSrc}
       />
-      <div className="movie-banner-card__inner">
+      <div className={classNames('movie-banner-card__inner', {
+        'movie-banner-card__inner--small-padding-y': isSmallPaddingY,
+      })}>
         <div className="movie-banner-card__body">
           <TitleTag
             className="movie-banner-card__title h3"

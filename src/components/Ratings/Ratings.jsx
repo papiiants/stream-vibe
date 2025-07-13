7eb19a62dd2e984ebc5a4242ba1343a0f4881133
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 18:55:56 +0200

    Add Ratings component

---
+++ src/components/Ratings/Ratings.jsx
@@ -0,0 +1,26 @@
+import './Ratings.scss'
+import RatingView from '@/components/RatingView'
+
+const Ratings = (props) => {
+  const {
+    items = [],
+  } = props
+
+  return (
+    <div className="ratings">
+      <ul className="rating__list">
+        {items.map(({ title, ratingValue }, index) => (
+          <li className="rating__item" key={index}>
+            <h4 className="ratings__title h6">{title}</h4>
+            <RatingView
+              value={ratingValue}
+              lagel={ratingValue}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Ratings

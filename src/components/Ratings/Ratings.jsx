9bb08502694fc70f2eb186c391aae6f1fd67f132
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 19:05:31 +0200

    Fix Rating component

--- src/components/Ratings/Ratings.jsx
+++ src/components/Ratings/Ratings.jsx
@@ -8,9 +8,9 @@ const Ratings = (props) => {
 
   return (
     <div className="ratings">
-      <ul className="rating__list">
+      <ul className="ratings__list">
         {items.map(({ title, ratingValue }, index) => (
-          <li className="rating__item" key={index}>
+          <li className="ratings__item" key={index}>
             <h4 className="ratings__title">{title}</h4>
             <RatingView
               value={ratingValue}

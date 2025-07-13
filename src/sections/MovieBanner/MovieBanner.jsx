8f32a4aa94917d28158cb5c29f204a5e2c887689
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 10:38:45 +0200

    Add title id for MovieBanner component

--- src/sections/MovieBanner/MovieBanner.jsx
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -10,7 +10,8 @@ const MovieBanner = () => {
       aria-labelledby={titleId}
     >
       <MovieBannerCard
-        titile="Kantara"
+        title="Kantara"
+        titleId={titleId}
         TitleTag="h1"
         description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
         imgSrc="/src/assets/images/movie-banner/2.jpg"

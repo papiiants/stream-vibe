6f2fe86f22859b50878cfee00aff0f4cb8ba7630
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 16:11:28 +0200

    Fix add reviewItems

--- src/sections/MovieDetails/MovieDetails.jsx
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -3,6 +3,7 @@ import SliderNavigation from "@/components/Slider/components/SliderNavigation";
 import Slider from "@/components/Slider";
 import PersonCard from "@/components/PersonCard";
 import Button from '@/components/Button'
+import ReviewCard from '@/components/ReviewCard'
 
 const MovieDetails = () => {
   const titleId = 'movie-details-title'
@@ -73,6 +74,60 @@ const MovieDetails = () => {
     {
       imgSrc: '/src/assets/images/people/8.jpg',
       imgAlt: 'Hampur Shampur',
+    },
+  ]
+
+  const reviewItems = [
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: '4.5',
+    },
+    {
+      name: 'Swaraj',
+      subtitle: 'From India',
+      description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+      ratingValue: '5',
+    },
+
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: '4.5',
+    },
+    {
+      name: 'Swaraj',
+      subtitle: 'From India',
+      description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+      ratingValue: '5',
+    },
+
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: '4.5',
+    },
+    {
+      name: 'Swaraj',
+      subtitle: 'From India',
+      description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+      ratingValue: '5',
+    },
+
+    {
+      name: 'Aniket Roy',
+      subtitle: 'From India',
+      description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+      ratingValue: '4.5',
+    },
+    {
+      name: 'Swaraj',
+      subtitle: 'From India',
+      description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+      ratingValue: '5',
     },
   ]
 
@@ -164,7 +219,9 @@ const MovieDetails = () => {
               },
             }}
           >
-
+            {reviewItems.map((reviewItem, index) => (
+              <ReviewCard {...reviewItem} key={index} />
+            ))}
           </Slider>
         </div>
       </div>

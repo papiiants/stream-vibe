0192277c250be6976d3da7f88bca8a973c94ef47
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 13:49:00 +0200

    Add small padding y prop to MovieBanner component

--- src/sections/MovieBanner/MovieBanner.jsx
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -15,6 +15,7 @@ const MovieBanner = () => {
         TitleTag="h1"
         description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
         imgSrc="/src/assets/images/movie-banner/2.jpg"
+        isSmallPaddingY={true}
       />
     </section>
   )

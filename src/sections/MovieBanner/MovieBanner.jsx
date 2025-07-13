27eee51d8c46779df567289a86ad7b266125038f
Dmytro Papiiants <papiyants@gmail.com>
2025-07-13 10:31:20 +0200

    Add MovieBanner component

---
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -0,0 +1,22 @@
+import MovieBannerCard from "@/components/MovieBannerCard";
+
+const MovieBanner = () => {
+
+  const titleId = 'movie-banner-title'
+
+  return (
+    <section
+      className="container"
+      aria-labelledby={titleId}
+    >
+      <MovieBannerCard
+        titile="Kantara"
+        TitleTag="h1"
+        description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
+        imgSrc="/src/assets/images/movie-banner/2.jpg"
+      />
+    </section>
+  )
+}
+
+export default MovieBanner

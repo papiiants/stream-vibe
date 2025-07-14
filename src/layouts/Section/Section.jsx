a0e2be9e0db8bf6a4df471c3628cc4865a27e191
Dmytro Papiiants <papiyants@gmail.com>
2025-07-14 10:35:10 +0200

    Fix section layout

--- src/layouts/Section/Section.jsx
+++ src/layouts/Section/Section.jsx
@@ -17,27 +17,29 @@ const Section = (props) => {
       className={classNames(className, 'section container')}
       aria-label={titleId}
     >
-      <header className="section__header">
-        <div className="section__info">
-          <h2 className="section__title h3" id={titleId}>
-            {title}
-          </h2>
-          {description && (
-            <div className="section__description">
-              <p>{description}</p>
+      {title && (
+        <header className="section__header">
+          <div className="section__info">
+            <h2 className="section__title h3" id={titleId}>
+              {title}
+            </h2>
+            {description && (
+              <div className="section__description">
+                <p>{description}</p>
+              </div>
+            )}
+          </div>
+          {actions && (
+            <div
+              className={classNames('section__actions', {
+                'hidden-mobile': isActionsHiddenOnMobile,
+              })}
+            >
+              {actions}
             </div>
           )}
-        </div>
-        {actions && (
-          <div
-            className={classNames('section__actions', {
-              'hidden-mobile': isActionsHiddenOnMobile,
-            })}
-          >
-            {actions}
-          </div>
-        )}
-      </header>
+        </header>
+      )}
       <div className="section__body">
         {children}
       </div>

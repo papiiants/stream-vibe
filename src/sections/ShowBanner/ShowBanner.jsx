a759459cdcb3b8aa380b3929b3ab8ff67276ead9
Dmytro Papiiants <papiyants@gmail.com>
2025-07-14 11:33:20 +0200

    Add MovieBannerCard component to ShowBanner section

--- src/sections/ShowBanner/ShowBanner.jsx
+++ src/sections/ShowBanner/ShowBanner.jsx
@@ -1,12 +1,22 @@
-const ShowBanner = (props) => {
-  const {
+import MovieBannerCard from '@/components/MovieBannerCard'
 
-  } = props
+const ShowBanner = () => {
+  const titleId = 'show-banner-title'
 
   return (
-    <div>
-      ShowBanner
-    </div>
+    <section
+      className="container"
+      aria-labelledby={titleId}
+    >
+      <MovieBannerCard
+        title="Stranger Things"
+        description="When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl."
+        TitleTag="h1"
+        titleId={titleId}
+        imgSrc="/src/assets/images/movie-banner/3.jpg"
+        isSmallPaddingY
+      />
+    </section>
   )
 }
 

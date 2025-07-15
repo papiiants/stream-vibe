0f21cd474835540be0f2bd791f55cf47d87d8551
Dmytro Papiiants <papiyants@gmail.com>
2025-07-15 15:19:14 +0200

    Add videoPlayerCollection module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import OverlayMenu from "@/Modules/OverlayMenu"
-import SliderCollection from "@/Modules/SliderCollection"
-import TabsCollection from "@/modules/TabsCollection";
+import OverlayMenu from '@/Modules/OverlayMenu'
+import SliderCollection from '@/Modules/SliderCollection'
+import TabsCollection from '@/modules/TabsCollection'
+import VideoPlayerCollection from '@/modules/videoPlayerCollection'
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollection()
+new VideoPlayerCollection()

41dfbe35311ef201ec20fd40eccc41c2ad3e8e22
Dmytro Papiiants <papiyants@gmail.com>
2025-07-15 19:50:05 +0200

    Fix Support section

--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -10,7 +10,14 @@ const Support = () => {
       aria-labelledby={titleId}
     >
       <div className="support__body">
-        <div className="support__info"></div>
+        <div className="support__info">
+          <h1 className="support__title h2" id={titleId}>
+            Welcome to our support page!
+          </h1>
+          <div className="support__description">
+            <p>We're here to help you with any problems you may be having with our product.</p>
+          </div>
+        </div>
         <Image
           className="support__image"
           src="/src/assets/images/support/1.png"

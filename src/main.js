1cbd92ffc11368ca6351069ee811ca9b2e9c3c37
Dmytro Papiiants <papiyants@gmail.com>
2025-07-16 12:01:57 +0200

    Fix add inputMaskCollection to main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import OverlayMenu from '@/Modules/OverlayMenu'
 import SliderCollection from '@/Modules/SliderCollection'
 import TabsCollection from '@/modules/TabsCollection'
 import VideoPlayerCollection from '@/modules/videoPlayerCollection'
+import InputMaskCollection from '@/modules/inputMaskCollection'
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollection()
 new VideoPlayerCollection()
+new InputMaskCollection()

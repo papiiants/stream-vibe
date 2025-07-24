2d8b06366e01595c83b8b72d6d903bd7a4106dcc
Dmytro Papiiants <papiyants@gmail.com>
2025-07-24 10:44:22 +0200

    Fix change base url to /stream-vibe/ in Banner section

--- src/sections/Banner/Banner.jsx
+++ src/sections/Banner/Banner.jsx
@@ -22,7 +22,7 @@ const Banner = () => {
         <Button
           className="banner__button"
           label="Start a Free Trail"
-          href="/subscriptions"
+          href="/stream-vibe/subscriptions"
         />
       </div>
     </Section>
